bdb127bd756274ed9f33e1c52fc76f8e9834c192
Thomas Margosian <tmargo101@gmail.com>
2021-03-16 18:21:16 -0400

    Created project

---
+++ TheKnow/TheKnow/TheKnowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheKnowApp.swift
+//  TheKnow
+//
+//  Created by Tom Margosian on 3/16/21.
+//
+
+import SwiftUI
+
+@main
+struct TheKnowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
