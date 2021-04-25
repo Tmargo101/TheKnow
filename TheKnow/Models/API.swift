8aab6cac6b1d1847706aa200fa3289ed54be11f1
Thomas Margosian <tmargo101@gmail.com>
2021-04-25 13:01:48 -0400

    Removed prototype API code.

--- TheKnow/Models/API.swift
+++ TheKnow/Models/API.swift
@@ -15,26 +15,5 @@ struct Routes {
 }
 
 class API {
-    
-    static let shared = API()
-    
-    func getLists(completion: @escaping ([PlaceList]?) -> Void) {
-        print(Routes.GET_LISTS)
-        AF.request(Routes.GET_LISTS).responseJSON { (response) in
-            
-            guard let data = response.data else {
-                completion(nil)
-                return
-            }
-            
-            do {
-                let placeLists = try JSONDecoder().decode([PlaceList].self, from: data)
-                completion(placeLists)
-            } catch {
-                completion(nil)
-                print(error)
-            }
-        }
-    }
 
 }
--- TheKnow/Views/ContentView.swift
+++ TheKnow/Views/ContentView.swift
@@ -10,33 +10,9 @@ import Alamofire
 
 struct ContentView: View {
     
-    let api = API.shared
-    
-    @State var placeLists: [PlaceList] = []
-    
-    init() {
-        
-        api.getLists { (returnedLists) in
-            guard let safeLists = returnedLists else {
-                // handle error
-                return
-            }
-            self.placeLists = safeLists
-        }
-        
-    }
     
     var body: some View {
-        NavigationView {
-            List(0..<placeLists.count) { placeList in
-                NavigationLink(
-                    destination: Text("Destination"),
-                    label: {
-                        Text("Navigate")
-                    })
-            }
-        }
-        .navigationTitle("The Know")
+        
     }
 }
 
